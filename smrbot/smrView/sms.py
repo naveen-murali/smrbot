2082f0ad365aaa1a7fddcdea964e02852c013b47
Naveen Murali <naveenmurali1211@gmail.com>
2021-02-24 13:23:55 +0530

    Brand new UI is added and QR-code function is changed. --Naveen Murali

--- smrbot/smrView/sms.py
+++ smrbot/smrView/sms.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
 
 account_sid = "AC54a6f0137ad395348284281c825268f8"
-auth_token = "89ecf74e03a45be98b01a01318a3165d"
+# auth_token = "58b61dc8f8772f37e7efb4bbbeb9f2fd"
 
 state = {
     "sms": None
@@ -13,3 +13,6 @@ def connect():
 def sendSMS(phoneNum, msg):
     state['sms'].messages.create(from_="+12058810586", body=msg, to=phoneNum)
 
+if __name__ == "__main__":
+    connect()
+    sendSMS("+919072767130", "hi hello!!")
